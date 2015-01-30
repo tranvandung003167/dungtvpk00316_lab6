745caa421be08e0d1f93c35d2d360c3805f3b514
tranvandung003167 <dungtvpk00316@fpt.edu.vn>
2015-01-30 14:54:31 +0700

    https://github.com/tranvandung003167/dungtvpk00316_lab6.git
    lab4

---
+++ dungtvpk00316/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Xin chào Dungtvpk00316")
+    End Sub
+End Class
